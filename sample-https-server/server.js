d29add3e6bba1732eaafd430f5b96ade464f7780
Ariel Bravo Ayala <2177550+ambravo@users.noreply.github.com>
2023-02-27 08:23:48 -0300

    Readme update

--- sample-https-server/server.js
+++ sample-https-server/server.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const options = {
   // The following (updated) files are to be downloaded from certs.a0.gg.
   // The key and cert provided in this repo might be expired
-  key: fs.readFileSync('_privkey.pem'), 
-  cert: fs.readFileSync('_fullchain.pem') 
+  key: fs.readFileSync('_privkey.pem'), // https://certs.a0.gg/download?key=_privkey.pem
+  cert: fs.readFileSync('_fullchain.pem') // https://certs.a0.gg/download?key=_fullchain.pem
 };
 
 const server = https.createServer(options, (req, res) => {
@@ -15,5 +15,5 @@ const server = https.createServer(options, (req, res) => {
 });
 
 server.listen(3443, () => {
-  console.log('Server listening on port https://local.a0.gg:3443');
+  console.log('Server listening on https://local.a0.gg:3443');
 });
