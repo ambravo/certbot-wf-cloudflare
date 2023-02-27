c28fa8edb9aa727fe0e2fe895f403c08b1df8ccd
Ariel Bravo Ayala <2177550+ambravo@users.noreply.github.com>
2023-02-27 08:17:25 -0300

    Readme and sample app

--- sample-https-server/server.js
+++ sample-https-server/server.js
@@ -2,8 +2,10 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('_privkey.pem'), //You download this file from certs.a0.gg
-  cert: fs.readFileSync('_fullchain.pem') //You download this file from certs.a0.gg
+  // The following (updated) files are to be downloaded from certs.a0.gg.
+  // The key and cert provided in this repo might be expired
+  key: fs.readFileSync('_privkey.pem'), 
+  cert: fs.readFileSync('_fullchain.pem') 
 };
 
 const server = https.createServer(options, (req, res) => {
@@ -13,5 +15,5 @@ const server = https.createServer(options, (req, res) => {
 });
 
 server.listen(3443, () => {
-  console.log('Server listening on port 3443');
+  console.log('Server listening on port https://local.a0.gg:3443');
 });
